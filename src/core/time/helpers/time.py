b0905d399292b513afb2ea7582237d0cb2d18a1d
Jakub <kubus691234@wp.pl>
2026-02-22 16:41:53 +0100

    Modify month_days function

--- src/core/time/helpers/time.py
+++ src/core/time/helpers/time.py
@@ -1,11 +1,5 @@
 from datetime import datetime, time, date
-
-
-def month_days(month:date) -> int:
-    m = month
-    if m in [1, 3, 5, 7, 8, 10, 12]:
-        return 31
-    elif m in [4, 6, 9, 11]:
-        return 30
-    else:
-        return 28
+from calendar import monthrange
+from typing import Tuple
+def month_days(month:date) -> Tuple[int, int]:
+    return monthrange(month.year, month.month)[1]
