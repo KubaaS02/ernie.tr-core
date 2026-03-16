a56380051e8c3f7091513bbcf98615c5b5da6065
Jakub <kubus691234@wp.pl>
2026-03-16 19:05:35 +0100

    Modify task model

--- src/models/task.py
+++ src/models/task.py
@@ -8,34 +8,48 @@ class Task:
     Model reprezentujący pojedyncze zadanie (task).
     
     Attributes:
-        task_id: Unikatowy identyfikator zadania
+        task_id: Unikatowy identyfikator taska
         task_start: Data i czas rozpoczęcia pracy (format: YYYY-MM-DD HH:MM lub HH:MM:SS)
         task_stop: Data i czas zakończenia pracy (format: YYYY-MM-DD HH:MM lub HH:MM:SS)
         comment: Opis/nazwa projektu
         status: Status płatności (Zapłacone, Oczekuje, W trakcie)
         payment_date: Data faktycznej płatności (opcjonalnie)
+        exchange_rate_eur: Kurs euro
+        duration_min: Czas trwania taska
+        payment_date: Data zapłacenia za taska
+        rate_pln_per_h: Godzinowa stawka za zadanie w złotówkach
+        description: Opis taska
+        cost_approx_pln: Przewidywany koszt w złotówkach
+        cost_approx_eur: Przewidywany koszt w euro
+        cost_actual_pln: Faktyczny koszt w złotówkach
+        cost_actual_eur: Faktyczny koszt w euro
+        title: Tytuł taska
+        task_date: Data taska
+        created_at: Data stworzenia taska
+        updated_at: Data zaaktualizowania taska
+        is_locked: Informacja, czy task jest zablokowany, czy nie
+        diff: Różnica pieniężna taska
     """
     
     task_start: datetime
     task_stop: datetime
     task_id: str
-    title: str
-    task_date: date
-    created_at: datetime
-    updated_at: datetime
-    is_locked: bool
     comment: str = ""
     exchange_rate_eur: float = 0.0
     duration_min: int = 1
     status: Literal["Zapłacone", "Oczekuje", "W trakcie"] = "Oczekuje"
     payment_date: Optional[datetime] = None
     rate_pln_per_h: Optional[float] = None
-    
     description: Optional[str] = None
     cost_approx_pln: Optional[float] = None
     cost_approx_eur: Optional[float] = None
     cost_actual_pln: Optional[float] = None
     cost_actual_eur: Optional[float] = None
+    title: Optional[str] = None
+    task_date: Optional[date] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    is_locked: Optional[bool] = None
     diff: float | None = None
     def __post_init__(self) -> None:
         """Walidacja danych po inicjalizacji"""
