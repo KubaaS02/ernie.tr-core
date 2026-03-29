dcc46cd9082a18adeda48737ee42f47c3eb04ce5
Jakub <kubus691234@wp.pl>
2026-03-29 18:43:05 +0200

    Modify project  model

--- src/models/project.py
+++ src/models/project.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from dataclasses import dataclass
 
+
 @dataclass
 class project:
     """
     Model reprezentujący jeden projekt (project)
-    
+
     Attributes:
         project_id: Unikatowy identyfikator projektu
         user_id: Identyfikator użytkownika
@@ -15,11 +16,27 @@ class project:
         created_at: Data stworzenia projektu
         is_active: Informacja, czy dany projekt jest aktywny
     """
-    
+
     project_id: str
     user_id: str
     name: str
     description: str
-    rate_project_pln_per_h: float = None
     created_at: datetime
     is_active: bool
+    rate_project_pln_per_h: float = 0.0
+
+    def is_project_id_unique(self):
+        raise NotImplementedError(
+            "This validation method must be implemented after succes bridge with Database")
+
+    def correct_rate_project_pln_per_h(self):
+        raise NotImplementedError(
+            "This validation method must be implemented after succes bridge with Database")
+
+    def created_at_is_real(self):
+        raise NotImplementedError(
+            "This validation method must be implemented after succes bridge with Database")
+
+    def is_active_have_only_True_or_False(self):
+        raise NotImplementedError(
+            "This validation method must be implemented after succes bridge with Database")
