6c88f38f4dff91ea7ff95ef6376812b6599da45a
Jakub <kubus691234@wp.pl>
2026-05-29 17:10:17 +0200

    Modify BusinessError

--- src/exceptions/handler_error.py
+++ src/exceptions/handler_error.py
@@ -13,7 +13,7 @@ class BusinessError(Exception):
     error_id: Optional[str] = "ERR-GENERIC"
     error_code: Optional[str] = "GENERIC_ERROR"
     http_status: Optional[int] = 500
-    default_message: Optional[str] = "Unexcepted business error"
+    default_message: Optional[str] = "Unexpected business error"
 
     _registered_ids: set[str] = set()
 
@@ -21,19 +21,21 @@ class BusinessError(Exception):
         super().__init_subclass__()
         if getattr(cls, "abstract", False):
             return
+        cls.abstract = False
         if not hasattr(cls, "error_id") or cls.error_id == "ERR-GENERIC":
             raise TypeError(
                 f"{cls.__name__} must define his it's own error_id")
         if cls.error_id in cls._registered_ids:
             raise ValueError(f"Duplicate error_id detected:{cls.error_id}")
-        
+
         cls._registered_ids.add(str(cls.error_id))
 
-    def __init__(self, message: str | None = None, details: dict | None = None, error_code: str | None = None):
-        super().__init__(message)
-        self.message = message or self.default_message
+    def __init__(self, message: str | None = None, details: Optional[dict] = None, error_code: str | None = None):
+        actual_message = message or self.default_message
+        super().__init__(actual_message)
+        self.message = actual_message
         self.details = details or {}
-        self.error_code = error_code
+        self.error_code = error_code or self.error_code
 
     def to_dict(self) -> dict:
         return {
@@ -42,4 +44,3 @@ class BusinessError(Exception):
             "message": self.message,
             "details": self.details
         }
-        
