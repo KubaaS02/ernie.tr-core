5e133ccafee6c2da4396a6eb75d37be93abea7a4
Jakub <kubus691234@wp.pl>
2026-06-15 18:48:33 +0200

    Add class TaskOverlapError

--- src/exceptions/time_errors.py
+++ src/exceptions/time_errors.py
@@ -9,3 +9,11 @@ class InvalidTaskTimeRangeError(BusinessError):
     error_code: Optional[str] = "invalid_task_time_range"
     http_status: Optional[int] = 400
     default_message: Optional[str] = "The start time of a task cannot be later than stop time"
+
+class TaskOverlapError(BusinessError):
+    
+    error_id: Optional[str] = "ERR-TIME-02"
+    error_code: Optional[str] = "task_overlap"
+    http_status: Optional[int] = 409
+    default_message: Optional[str] = "One task overlaps with another"
+    #TODO: Zrobić commita; "Add class TaskOverlapError"
