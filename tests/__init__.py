6213fd4be051fb1d3e1e0c8916bee84d66c1cb96
Jakub <kubus691234@wp.pl>
2026-06-15 18:49:19 +0200

    Import TaskOverlapError class

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,4 @@ from core.salary.task_salary import get_approx_cost_pln, get_approx_cost_from_pl
 from exceptions.handler_error import BusinessError
 from core.time.task_duration import get_task_time_duration, get_tasks_time_one_day, get_tasks_time_one_month
 from models.task import Task
-from exceptions.time_errors import InvalidTaskTimeRangeError
+from exceptions.time_errors import InvalidTaskTimeRangeError, TaskOverlapError
