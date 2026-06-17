18f4b71204d7db0e04e57fb6776a5ab951a1e727
Jakub <kubus691234@wp.pl>
2026-06-17 17:31:02 +0200

    Clean up code

--- src/exceptions/finance_errors.py
+++ src/exceptions/finance_errors.py
@@ -4,11 +4,11 @@ from typing import Optional
 
 """Grupy błędów związane z finansami"""
 
+
 class InvalidExchangeRateError(BusinessError):
     """Klasa błędu, który powinien zostać wywołany gdy kurs wymiany waluty będzie nieodpowiedni, tzn <=0"""
-    
+
     error_id: Optional[str] = "ERR-FIN-01"
     error_code: Optional[str] = "invalid_exchange_rate"
     http_status: Optional[int] = 422
     default_message: Optional[str] = "The exchange rate is incorrect, <=0"
-    #TODO sprawdzić testy
