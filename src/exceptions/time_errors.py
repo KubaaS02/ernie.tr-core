3f5dc49bad9edc540c90ad0e1bc90a2a942f3a80
Jakub <kubus691234@wp.pl>
2026-06-17 17:29:53 +0200

    Add new class TaskOverlError and and class descriptions

--- src/exceptions/time_errors.py
+++ src/exceptions/time_errors.py
@@ -2,19 +2,20 @@ from exceptions import BusinessError
 from typing import Optional
 
 """Grupa błędów związanych z harmonogramem i czasem trwania"""
-#TODO: dopisać opisy błędów
+
+
 class InvalidTaskTimeRangeError(BusinessError):
-    """Grupa błędów związanych z harmonogramem i czasem trwania"""
+    """Klasa błędu związanego z niepoprawnym czasem zadania. Task_start > Task_stop"""
 
     error_id: Optional[str] = "ERR-TIME-01"
     error_code: Optional[str] = "invalid_task_time_range"
     http_status: Optional[int] = 400
     default_message: Optional[str] = "The start time of a task cannot be later than stop time"
 
+
 class TaskOverlapError(BusinessError):
-    
+    """Klasa błędu związanego z pokrywającym się czasem zadań. Nowe zadanie czasowo pokrywa się z już istniejącym zadaniem użytkownika"""
     error_id: Optional[str] = "ERR-TIME-02"
     error_code: Optional[str] = "task_overlap"
     http_status: Optional[int] = 409
     default_message: Optional[str] = "One task overlaps with another"
-    #TODO: Zrobić commita; "Add class TaskOverlapError"
