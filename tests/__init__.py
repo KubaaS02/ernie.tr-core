07ce822abe1e26d6adcd7fffb24247d7dd626ab4
Jakub <kubus691234@wp.pl>
2026-06-20 20:25:19 +0200

    Add TaskLockedError class to import

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,3 +5,4 @@ from core.time.task_duration import get_task_time_duration, get_tasks_time_one_d
 from models.task import Task
 from exceptions.time_errors import InvalidTaskTimeRangeError, TaskOverlapError
 from exceptions.finance_errors import InvalidExchangeRateError, InvalidHourdlyRateError
+from exceptions.authorization_errors import  TaskLockedError
