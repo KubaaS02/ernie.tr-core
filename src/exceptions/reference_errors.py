841947834f1ef64862f25c0a949a7726c2d83f8a
Jakub <kubus691234@wp.pl>
2026-06-21 19:47:19 +0200

    Add EntityNotFoundError class

--- src/exceptions/reference_errors.py
+++ src/exceptions/reference_errors.py
@@ -9,3 +9,10 @@ class ProjectInUseError(BusinessError):
     error_code: Optional[str] = "project_in_use"
     http_status: Optional[int] = 409
     default_message: Optional[str] = "You cannot delete the project. It is in use in another task."
+
+
+class EntityNotFoundError(BusinessError):
+    error_id: Optional[str] = "ERR-REF-02"
+    error_code: Optional[str] = "entity_not_found"
+    http_status: Optional[int] = 404
+    default_message: Optional[str] = "The entity was not found."
