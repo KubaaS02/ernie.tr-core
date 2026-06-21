f5275f6d20e3ad93fc137e3f68ed33d6f6774886
Jakub <kubus691234@wp.pl>
2026-06-21 14:24:35 +0200

    Add to imports ProjectInUseError and project classes

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,3 +6,5 @@ from models.task import Task
 from exceptions.time_errors import InvalidTaskTimeRangeError, TaskOverlapError
 from exceptions.finance_errors import InvalidExchangeRateError, InvalidHourdlyRateError
 from exceptions.authorization_errors import  TaskLockedError
+from exceptions.reference_errors import  ProjectInUseError
+from models.project import project
