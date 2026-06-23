8294822dafb7142eca50011e0f52d8bb45bb2d1c
Jakub <kubus691234@wp.pl>
2026-06-23 16:07:07 +0200

    Add MissingCalculationDataErrors to imports

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,6 +5,7 @@ from core.time.task_duration import get_task_time_duration, get_tasks_time_one_d
 from models.task import Task
 from exceptions.time_errors import InvalidTaskTimeRangeError, TaskOverlapError
 from exceptions.finance_errors import InvalidExchangeRateError, InvalidHourdlyRateError
-from exceptions.authorization_errors import  TaskLockedError
-from exceptions.reference_errors import  ProjectInUseError, EntityNotFoundError
+from exceptions.authorization_errors import TaskLockedError
+from exceptions.reference_errors import ProjectInUseError, EntityNotFoundError
 from models.project import project
+from exceptions.data_errors import MissingCalculationDataError
