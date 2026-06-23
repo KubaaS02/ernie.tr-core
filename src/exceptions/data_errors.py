99ad5d92b7a94f2cd3e5fa2844a33743df9f386f
Jakub <kubus691234@wp.pl>
2026-06-23 16:07:32 +0200

    Add MissingCalculationDataError class

---
+++ src/exceptions/data_errors.py
@@ -0,0 +1,13 @@
+from exceptions import BusinessError
+from typing import Optional
+
+"""Błędy spójności danych wejściowych, np. brak wymaganych pól do wykonania kalkulacji."""
+
+
+class MissingCalculationDataError(BusinessError):
+    """Błąd związany z brakującymi danymi potrzebnymi do kalkulacji kosztu"""
+
+    error_id: Optional[str] = "ERR-DATA-01"
+    error_code: Optional[str] = "missing_calculation_data"
+    http_status: Optional[int] = 400
+    default_message: Optional[str] = "Brak danych potrzebnych do obliczenia kosztów"
