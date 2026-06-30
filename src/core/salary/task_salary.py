d7b67997844cd3f4f45dee7bd929ec6dff674b22
Jakub <kubus691234@wp.pl>
2026-06-30 22:32:58 +0200

    Add error handling using new exceptions

--- src/core/salary/task_salary.py
+++ src/core/salary/task_salary.py
@@ -1,5 +1,7 @@
 from models.task import Task
 import requests
+from exceptions.finance_errors import InvalidHourdlyRateError, InvalidExchangeRateError
+
 
 def get_approx_cost_pln(task: Task, rate_pln_per_h: float) -> float:
     """
@@ -10,10 +12,10 @@ def get_approx_cost_pln(task: Task, rate_pln_per_h: float) -> float:
         rate_pln_per_h: Kwota na godzinę w złotówkach (float)
 
     Returns:
-        PRzybliżony koszt tasku w PLN (float)
+        Przybliżony koszt tasku w PLN (float)
 
     Raises:
-        ValueError: Stawka za godzinę musi być większa niż 0
+        InvalidHourdlyRateError: Stawka za godzinę musi być większa niż 0
 
     Examples:
         # Stawka za godzinę jest ustalona przy tworzeniu Taska
@@ -21,7 +23,7 @@ def get_approx_cost_pln(task: Task, rate_pln_per_h: float) -> float:
         >>> approx_cost_pln(task, rate_pln_per_h=999)
         (120.0)
 
-        # Stawka za godzinę jest nie ustalona przy tworzeniu Taska. Funkcja bierze kwotę na godzinę z argumentu funkcji
+        # Stawka za godzinę nie jest ustalona przy tworzeniu Taska. Funkcja bierze kwotę na godzinę z argumentu funkcji
         >>> task = Task(task_id="1", task_start=datetime(2025, 11, 1, 9, 0),  task_stop=datetime(2025, 11, 1, 11, ))
         >>> approx_cost_pln(task, rate_pln_per_h=120)
         (240.0)
@@ -29,7 +31,11 @@ def get_approx_cost_pln(task: Task, rate_pln_per_h: float) -> float:
     rate = task.rate_pln_per_h if task.rate_pln_per_h is not None else rate_pln_per_h
 
     if rate <= 0:
-        raise ValueError("Stawka za godzinę musi być większa niż 0")
+        raise InvalidHourdlyRateError(
+            details={
+                "hourly_rate": rate
+            }
+        )
 
     duration_hours = task.duration_min / 60
     cost_approx_pln_raw = duration_hours * rate
@@ -48,7 +54,7 @@ def get_approx_cost_from_pln_to_euro(task: Task) -> float:
         Przybliżony koszt tasku w EUR (float)
 
     Raises:
-        ValueError: Kurs musi być większy niż 0
+        InvalidExchangeRateError: Kurs przewalutowania z PLN na EUR musi być > 0
 
     Example:
         >>> task = Task(task_id="1", task_start=datetime(2025, 11, 1, 9, 0),  task_stop=datetime(2025, 11, 1, 11, ), cost_approx_pln = 1500)
@@ -60,14 +66,19 @@ def get_approx_cost_from_pln_to_euro(task: Task) -> float:
         raise ValueError("Brak cost_approx_pln")
 
     if rate_euro_pln <= 0:
-        raise ValueError("Kurs musi być większy niż 0")
+        raise InvalidExchangeRateError(
+            details={
+                "rate": rate_euro_pln
+            }
+        )
     cost_approx_eur_raw: float = task.cost_approx_pln / rate_euro_pln
     cost_approx_eur: float = round(cost_approx_eur_raw, 2)
     return cost_approx_eur
 
-def get_nbp_euro_rate(task:Task) -> float:
+
+def get_nbp_euro_rate(task: Task) -> float:
     exchange_rate: float = 0.0
-    
+
     def get_exchange_rate():
         #! TODO: Jak będzie udostępnione API do bazy danych to:
         #! Pobrac ostatni task, jeżeli jest to możliwe, to wziąć z niego kurs z pola exchange_rate_eur
@@ -75,16 +86,16 @@ def get_nbp_euro_rate(task:Task) -> float:
         #! jezeli brak global_exchange_rate_eur to zwracamy 0
         #! cała logika powinna być w funkcji wewnętrznej get_exchange_rate()
         pass
-    
+
     date = task.created_at
     url = f"https://api.nbp.pl/api/exchangerates/rates/A/EUR/{date}?format=json"
     try:
         resp = requests.get(url, timeout=10)
         resp.raise_for_status()
         data = resp.json()
-        
-        return round(float(data["rates"][0]["mid"]),2)
-    
+
+        return round(float(data["rates"][0]["mid"]), 2)
+
     except requests.exceptions.RequestException:
         #! TODO: Jak będzie udostępnione API do bazy danych to:
         #! Pobrac ostatni task, jeżeli jest to możliwe, to wziąć z niego kurs z pola exchange_rate_eur
@@ -102,5 +113,5 @@ def get_nbp_euro_rate(task:Task) -> float:
         #! jezeli brak global_exchange_rate_eur to zwracamy 0
         #! cała logika powinna być w funkcji wewnętrznej get_exchange_rate()
         exchange_rate = get_exchange_rate()
-            
+
     return exchange_rate
