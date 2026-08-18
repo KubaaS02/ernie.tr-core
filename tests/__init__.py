99d07f351890b7de6471f70c71a0227a3a0518f3
Jakub <kubus691234@wp.pl>
2026-08-18 12:58:08 +0200

    Add get_approx_day_cost function to imports

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,18 @@
 from models.task import Task
 from models.day import day
-from core.salary.task_salary import get_approx_cost_pln, get_approx_cost_from_pln_to_euro, get_actual_cost_pln, get_actual_cost_euro_from_pln, get_actual_approx_cost_diff, get_diff_day, get_diff_month
+from core.salary.task_salary import (
+    get_approx_cost_pln,
+    get_approx_cost_from_pln_to_euro,
+    get_actual_cost_pln,
+    get_actual_cost_euro_from_pln,
+    get_actual_approx_cost_diff,
+    get_diff_day, get_diff_month,
+    get_approx_day_cost)
 from exceptions.handler_error import BusinessError
-from core.time.task_duration import get_task_time_duration, get_tasks_time_one_day, get_tasks_time_one_month
+from core.time.task_duration import (
+    get_task_time_duration,
+    get_tasks_time_one_day,
+    get_tasks_time_one_month)
 from models.task import Task
 from exceptions.time_errors import InvalidTaskTimeRangeError, TaskOverlapError
 from exceptions.finance_errors import InvalidExchangeRateError, InvalidHourdlyRateError
